19e0b30b7f360171d2e4575e8ba43e234b7fb88f
andreabgurdulich <andreabgurdulich@gmail.com>
2016-09-27 19:58:20 -0300

    Clase Data Personaje terminada, tiene UN erro que no sé porque lo tira. Lo revisamos en clase.

--- src/data/dataPersonaje.java
+++ src/data/dataPersonaje.java
@@ -4,22 +4,43 @@ import java.sql.*;
 import entities.*;
 import util.ApplicationException;
 
-public class dataPersonaje {
+public class dataPersonaje{
 	
-public dataPersonaje(){
-}
+	//public dataPersonaje(){}
 
-public void add (Personaje p){
-	ResultSet rs = null;
-	PreparedStatement stmt = null;
-	
-	try{
-		stmt = FactoryConexion.getInstancia().getConn().prepareStatement("insert into personaje(vida,energia,defensa,evasion,puntosTotales)"+"values(?,?,?,?,?)", PreparedStatement.RETURN_GENERATED_KEYS);
-		//stmt.setInt(1, p.getVida());
-	}
-	finally{
-		//comment
-	}
-	
-}
+	public void add (Personaje p){
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+		
+		try{
+			stmt = FactoryConexion.getInstancia().getConn().prepareStatement("insert into personaje(vida,energia,defensa,evasion,puntosTotales)"+"values(?,?,?,?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, p.getId());
+			stmt.setInt(2, p.getVida());
+			stmt.setInt(3, p.getEnergia());
+			stmt.setInt(4, p.getDefensa());
+			stmt.setInt(5, p.getEvasion());
+			stmt.setInt(6, p.getPuntosTotales());
+			stmt.execute();
+			
+			rs = stmt.getGeneratedKeys();
+			if(rs!=null && rs.next()){
+				p.setId(rs.getInt(1));
+				}
+		}
+			catch(SQLException e){
+				e.printStackTrace();
+			} catch (ApplicationException e){
+				e.printStackTrace();
+			}
+		finally{
+				try{
+						if(rs!=null) rs.close();
+						if(stmt!=null) stmt.close();
+						FactoryConexion.getInstancia().releaseConn();
+					} catch(ApplicationException e){
+						e.printStackTrace();
+					} catch(SQLException e){
+						e.printStackTrace();
+						}
+				}
 }
